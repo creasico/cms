9f7b2d0c4df5af78dd247e9467c34aa72e67b661
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-05-22 05:37:13 +0700

    Clean up directories. Remove 'Listeners' and 'Policies' dir which is not in use (yet) & move abstract class under 'Events' and 'Jobs' dir to 'Support'
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/Support/Event.php
+++ app/Support/Event.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Events;
+namespace App\Support\Events;
 
 abstract class Event
 {
--- app/Support/Job.php
+++ app/Support/Job.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Jobs;
+namespace App\Support\Jobs;
 
 use Illuminate\Bus\Queueable;
 
