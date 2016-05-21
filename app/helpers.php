559b2afb4a0eeee7670d422c6e17ea9fda430911
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-05-22 02:47:31 +0700

    Add application helpers file
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ app/helpers.php
@@ -0,0 +1,42 @@
+<?php
+
+
+if (! function_exists('setting')) {
+    /**
+     * Get Setting value by $key
+     *
+     * @param  string $key     Setting key
+     * @param  string $default Default value
+     * @return mixed
+     */
+    function setting($key, $default = null)
+    {
+        $settings = Cache::rememberForever('settings', function () {
+            return [];
+        });
+
+        return isset($settings[$key]) ? $settings[$key] : $default;
+    }
+}
+
+if (! function_exists('upload_path')) {
+    /**
+     * Retrive upload_path.
+     *
+     * @param  string $path     Path suffix.
+     * @param  bool   $absolute Whether you want to get an absolute path.
+     * @return [type]
+     */
+    function upload_path($path = '', $absolute = false)
+    {
+        $path = 'uploads'.trim($path, '/');
+
+        if ($absolute) {
+            $disk  = config('filesystems.default');
+            $appFs = config('filesystems.disks.'.$disk.'.root');
+            $path  = $appFs.'/'.$path;
+        }
+
+        return $path;
+    }
+}
