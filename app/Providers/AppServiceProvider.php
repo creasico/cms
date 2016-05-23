5f7429bbc2cd8df5acb611e04f0eeeab42ffcde7
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-05-24 06:12:27 +0700

    Install sentry for development dependency
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Clockwork\Support\Laravel\ClockworkServiceProvider;
 use Fitztrev\QueryTracer\Providers\QueryTracerServiceProvider;
+use Sentry\SentryLaravel\SentryLaravelServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -42,6 +43,7 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->config['app.debug'] === true) {
             $providers[] = ClockworkServiceProvider::class;
             $providers[] = QueryTracerServiceProvider::class;
+            $providers[] = SentryLaravelServiceProvider::class;
         }
 
         return $providers;
