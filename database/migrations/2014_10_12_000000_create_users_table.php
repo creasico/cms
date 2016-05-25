f5b4f2027f2cafa28287985b6f0c401e457f8f7b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-05-25 13:06:57 +0700

    Update users migration table
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password', 63);
+            $table->string('display')->default('Anonymous');
+
+            $table->boolean('active')->default(false);
+            $table->integer('login_count')->default(0);
+            $table->dateTime('current_login_at')->nullable();
+            $table->string('current_login_ip')->default('');
+            $table->dateTime('last_login_at')->nullable();
+            $table->string('last_login_ip')->default('');
+
             $table->rememberToken();
             $table->timestamps();
         });
